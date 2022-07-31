2359c1c79758846d76d4847dec29093da7b28a4a
LiliyaFaizulina <104977350+LiliyaFaizulina@users.noreply.github.com>
2022-07-31 05:16:56 +0000

    Deploying to gh-pages from @ LiliyaFaizulina/team-project-5-element@1761b8b81a6326fdb89ffb597b4d0beba4927d4d 🚀

--- index.2574ccb2.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),o=document.querySelector("#page");e.addEventListener("click",(()=>{const s="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!s),t.classList.toggle("is-open"),o.classList.toggle("no-scroll")}))})();
-//# sourceMappingURL=index.2574ccb2.js.map
--- index.9417feca.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t,a;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),a=document.querySelector("#page"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),a.classList.toggle("no-scroll")}))}();
-//# sourceMappingURL=index.9417feca.js.map
