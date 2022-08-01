a818bf84bd9c46a8d45fe12ef968b0d400f5ffcc
Лілія <lily.fed85@gmail.com>
2022-08-01 09:47:06 +0300

    change structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const bodyRef = document.querySelector("#page");
-
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
-    bodyRef.classList.toggle("no-scroll");
-  });
-})();
-
 import './js/jquery';
 import './js/slick.min';
 import './js/script';
 import './js/modal';
-
-
+import './js/mobile-menu';
