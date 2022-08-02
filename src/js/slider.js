c45c616ef65ce8cadf0683adbd56be328a8e81b2
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2022-08-02 10:39:32 +0300

    add new style from slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,49 +1,28 @@
 $('.reviews__wrap').slick({
-   infinite: true,
-   slidesToShow: 1,
-   slidesToScroll:1, 
-   waitForAnimate: false,
-   asNavFor: '.reviews__list',
- });
- $('.reviews__list').slick({
-   infinite: true,
-   arrows: false,
-   slidesToShow: 7,
-   slidesToScroll: 1, 
-   waitForAnimate: false,
-   centerMode: true,
-   focusOnSelect: true,
-   asNavFor: '.reviews__wrap',
-   responsive: [
-      {
-         breakpoint: 1366,
-         settings: {
-           slidesToShow: 7,
-           slidesToScroll: 1
-         }
-       },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 7,
-          slidesToScroll: 1
-          
-        }
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  waitForAnimate: false,
+  asNavFor: '.reviews__list',
+});
+
+$('.reviews__list').slick({
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  waitForAnimate: false,
+  focusOnSelect: true,
+  asNavFor: '.reviews__wrap',
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        infinite: false,
       },
-      {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 3,
-           slidesToScroll: 1
-           
-         }
-       },
-      
-    ]
-   
- });
-
-
-
-
- 
+    },
+  ],
+});
