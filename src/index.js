82ff3f0b6a50c03109f4b46e1cc48b6f3adad06a
Лілія <lily.fed85@gmail.com>
2022-08-02 09:18:15 +0300

    add scroll-header js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/slider';
 import './js/modal';
 import './js/mobile-menu';
+import './js/header-scroll';
---
+++ src/js/header-scroll.js
@@ -0,0 +1,14 @@
+window.onscroll = () => changeHeaderBackground();
+
+function changeHeaderBackground() {
+  const header = document.getElementById("header");
+  const headerOffsetTrigger = header.offsetTop;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add("header--no-transparency");
+  } else {
+    header.classList.remove("header--no-transparency");
+  }
+}
+
